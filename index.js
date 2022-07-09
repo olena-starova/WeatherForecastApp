6feaf9e777ce864691aa0b694b1a3537482ea582
olena-starova <iverra@ukr.net>
2022-07-09 20:38:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
             forecastDay.weather[0].icon
           }@2x.png"
           alt=""
-          width="45"
+          width="50"
         />
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max">${Math.round(
