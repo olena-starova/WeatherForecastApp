ed0051bc7e6c6ee9a527541dcb197f49c0850924
olena-starova <iverra@ukr.net>
2022-07-09 18:50:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -43,25 +43,33 @@ function formatDate(date) {
 formatDate();
 
 function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
-  let forecastHTML = `<div class="row">`;
-  weekDays.forEach(function (weekDay) {
+  let forecastHTML = forecastHTML + `<div class="row">`;
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `
       <div class="col">
-        <div class="weather-forecast-date">${weekDay}</div>
+        <div class="weather-forecast-date">${forecastDay.dt}</div>
         <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           width="25"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max">${Math.round(
+            forecastDay.temp.max
+          )}</span>
+          <span class="weather-forecast-temperature-min">${Math.round(
+            forecastDay.temp.min
+          )}</span>
         </div>
       </div>
   `;
